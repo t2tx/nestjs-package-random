78b959a3acffa1d91bd92d477f49d917059b6925
Tei Jo <t2tx@icloud.com>
2021-12-26 00:24:59 +0900

    INIT: for publish

--- src/random-number.module.ts
+++ src/random-number.module.ts
@@ -1,5 +1,5 @@
-import { DynamicModule, Module } from "@nestjs/common";
-import { RandomNumberService } from "./random-number.service";
+import { DynamicModule, Module } from '@nestjs/common';
+import { RandomNumberService } from './random-number.service';
 
 @Module({})
 export class RandomNumberModule {
